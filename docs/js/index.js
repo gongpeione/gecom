ae257bda746a57f4e8eeed5726499756c82e4c63
Geeku <ggongpei@qq.com>
2017-01-05 22:03:28 +0800

    update page

--- docs/js/index.js
+++ docs/js/index.js
@@ -8,6 +8,16 @@ webpackJsonp([0,1],[
 	
 	__webpack_require__(3);
 	
+	__webpack_require__(9);
+	
+	__webpack_require__(11);
+	
+	__webpack_require__(13);
+	
+	__webpack_require__(15);
+	
+	__webpack_require__(17);
+	
 	console.log('src');
 
 /***/ },
@@ -25,10 +35,120 @@ webpackJsonp([0,1],[
 	
 	__webpack_require__(4);
 	
-	console.log('basic');
+	__webpack_require__(5);
+	
+	__webpack_require__(7);
 
 /***/ },
 /* 4 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(6);
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 7 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(8);
+
+/***/ },
+/* 8 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 9 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(10);
+	
+	console.log('data');
+
+/***/ },
+/* 10 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 11 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(12);
+	
+	console.log('form');
+
+/***/ },
+/* 12 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 13 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(14);
+	
+	console.log('navigation');
+
+/***/ },
+/* 14 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 15 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(16);
+	
+	console.log('notice');
+
+/***/ },
+/* 16 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+/* 17 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	__webpack_require__(18);
+	
+	console.log('others');
+
+/***/ },
+/* 18 */
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
